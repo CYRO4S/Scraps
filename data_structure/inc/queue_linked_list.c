4e34cfe4b7b653e2573d785bc6a81d2f0a9b72ae
Ralf Ren <me@ralf.ren>
2018-10-04 17:30:49 +0800

    CS97SI - queue implementation using linked list

--- data_structure/inc/queue_linked_list.c
+++ data_structure/inc/queue_linked_list.c
@@ -54,3 +54,24 @@ struct QueueNode* dequeue(LinkedQueue* queue)
         queue->rear = NULL;
     return temp;
 }
+
+// main function for testing
+/*
+int main(int argc, char* argv[])
+{
+    LinkedQueue *queue = createQueue();
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    dequeue(queue);
+    dequeue(queue);
+    enqueue(queue, 30);
+    enqueue(queue, 40);
+    enqueue(queue, 50);
+
+    struct QueueNode *node = dequeue(queue);
+    if (node != NULL)
+        printf("De-queued: %d", node->key);
+
+    return 0;
+}
+ */
